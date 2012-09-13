mozilla/BrowserQuest

Cleaning up build.sh
* Quoting all variables to avoid possible word splitting
* Sanity checking chdir(2) calls

bin/build.sh
@@ -1,30 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script to generate an optimized client build of BrowserQuest
 
 BUILDDIR="../client-build"
 PROJECTDIR="../client/js"
-CURDIR=$(pwd)
+CURDIR=$PWD
 
 
 echo "Deleting previous build directory"
-rm -rf $BUILDDIR
+rm -rf "$BUILDDIR"
 
 echo "Building client with RequireJS"
-cd $PROJECTDIR
+cd "$PROJECTDIR" || exit 1
 node ../../bin/r.js -o build.js
-cd $CURDIR
+cd "$CURDIR"
 
 echo "Removing unnecessary js files from the build directory"
-find $BUILDDIR/js -type f -not \( -name "game.js" -o -name "home.js" -o -name "log.js" -o -name "require-jquery.js" -o -name "modernizr.js" -o -name "css3-mediaqueries.js" -o -name "mapworker.js" -o -name "detect.js" -o -name "underscore.min.js" -o -name "text.js" \) -delete
+find "$BUILDDIR/js" -type f -not \( -name "game.js" -o -name "home.js" -o -name "log.js" -o -name "require-jquery.js" -o -name "modernizr.js" -o -name "css3-mediaqueries.js" -o -name "mapworker.js" -o -name "detect.js" -o -name "underscore.min.js" -o -name "text.js" \) -delete
 
 echo "Removing sprites directory"
-rm -rf $BUILDDIR/sprites
+rm -rf "$BUILDDIR/sprites"
 
 echo "Removing config directory"
-rm -rf $BUILDDIR/config
+rm -rf "$BUILDDIR/config"
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt $CURDIR
+mv "$BUILDDIR/build.txt" "$CURDIR"
 
 echo "Build complete"

tombertrand/nanobrowserquest

bin/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script to generate an optimized client build of BrowserQuest
 
@@ -6,15 +6,14 @@ TOPLEVELDIR="`dirname $0`/.."
 BUILDDIR="$TOPLEVELDIR/client-build"
 PROJECTDIR="$TOPLEVELDIR/client/js"
 
-
 echo "Deleting previous build directory"
-rm -rf $BUILDDIR
+rm -rf "$BUILDDIR"
 
 echo "Building client with RequireJS"
-node $TOPLEVELDIR/bin/r.js -o $PROJECTDIR/build.js
+node "$TOPLEVELDIR/bin/r.js" -o "$PROJECTDIR/build.js"
 
 echo "Removing unnecessary js files from the build directory"
-find $BUILDDIR/js -type f \
+find "$BUILDDIR/js" -type f \
   -not \( -name "game.js" \
   -o -name "home.js" \
   -o -name "log.js" \
@@ -28,12 +27,12 @@ find $BUILDDIR/js -type f \
   -delete
 
 echo "Removing sprites directory"
-rm -rf $BUILDDIR/sprites
+rm -rf "$BUILDDIR/sprites"
 
 echo "Removing config directory"
-rm -rf $BUILDDIR/config
+rm -rf "$BUILDDIR/config"
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt $TOPLEVELDIR
+mv "$BUILDDIR/build.txt" "$TOPLEVELDIR"
 
 echo "Build complete"