74fbb309694fcdb3b530f68d48017674e7f96b52
Tom Bertrand <tomlebrute@gmail.com>
2023-11-05 02:53:43 -0500

    Update worldserver.ts

--- server/js/worldserver.ts
+++ server/js/worldserver.ts
@@ -2801,7 +2801,7 @@ class World {
       // amount = generateRandomBananoAmount(attacker.network);
     }
 
-    itemName = generateDroppedItem();
+    itemName = generateDroppedItem() || itemName
 
     return itemName
       ? this.addItem(
