b3347a6987d011a8f46da3e018262712673388db
Tom Bertrand <tom@zapper.fi>
2024-01-12 13:52:29 -0500

    is near 32

--- server/js/player.ts
+++ server/js/player.ts
@@ -956,20 +956,20 @@ class Player extends Character {
           }
         }
       } else if (action === Types.Messages.AGGRO) {
-        // const mob = self.server.getEntityById(message[1]);
-        // if (mob && !self.isNear(mob, 16)) {
-        //   const until = 365 * 24 * 60 * 60 * 1000 + Date.now();
-        //   databaseHandler.banPlayerByIP({
-        //     admin: "auto-mod",
-        //     player: self,
-        //     reason: "cheating",
-        //     until,
-        //     message: "player AGGRO not near enemy ban",
-        //   });
-        //   postMessageToModeratorSupportChannel(`
-        //   :warning: **${self.name}**:warning: was banned for exploiting the AGGRO message`);
-        //   return;
-        // }
+        const mob = self.server.getEntityById(message[1]);
+        if (mob && !self.isNear(mob, 32)) {
+          const until = 365 * 24 * 60 * 60 * 1000 + Date.now();
+          databaseHandler.banPlayerByIP({
+            admin: "auto-mod",
+            player: self,
+            reason: "cheating",
+            until,
+            message: "player AGGRO not near enemy ban",
+          });
+          postMessageToModeratorSupportChannel(`
+          :warning: **${self.name}**:warning: was banned for exploiting the AGGRO message`);
+          return;
+        }
         console.info("AGGRO: " + self.name + " " + message[1]);
         if (self.move_callback) {
           self.server.handleMobHate(message[1], self.id, 5);
@@ -995,7 +995,7 @@ class Player extends Character {
           self.setTarget(mob);
           self.server.broadcastAttacker(self);
 
-          if (!self.isNear(mob, 16)) {
+          if (!self.isNear(mob, 32)) {
             const until = 365 * 24 * 60 * 60 * 1000 + Date.now();
             databaseHandler.banPlayerByIP({
               admin: "auto-mod",
@@ -1013,7 +1013,7 @@ class Player extends Character {
         console.info("HIT: " + self.name + " " + message[1]);
         var mob = self.server.getEntityById(message[1]);
 
-        if (!self.isNear(mob, 16)) {
+        if (!self.isNear(mob, 32)) {
           const until = 365 * 24 * 60 * 60 * 1000 + Date.now();
           databaseHandler.banPlayerByIP({
             admin: "auto-mod",
